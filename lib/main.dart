094109983d0f0ff132d21995f05dbefbd9aaf778
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-10-08 16:35:56 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:dart_openai/dart_openai.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:syncia/pages/home.dart';
 
 Future<void> main() async {
-  await dotenv.load(fileName: ".env");
+  await dotenv.load(fileName: ".env").then(
+      (_) => OpenAI.apiKey = dotenv.env['API_KEY']!
+  );
   runApp(const MyApp());
 }
 
@@ -13,7 +16,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      color: Colors.blue,
+      theme: ThemeData(useMaterial3: true,buttonTheme: const ButtonThemeData(buttonColor: Colors.blue,)),
       debugShowCheckedModeBanner: false,
       initialRoute: HomePage.id,
       routes: {
