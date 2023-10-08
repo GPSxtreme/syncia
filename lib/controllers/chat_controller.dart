f825a07de420a3061da60c1d80258849f6c20f3e
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-10-08 16:35:43 +0530

    Update chat_controller.dart

--- lib/controllers/chat_controller.dart
+++ lib/controllers/chat_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import '../models/chat_message.dart';
 import '../services/local_database_service.dart';
@@ -5,11 +6,14 @@ import '../services/local_database_service.dart';
 class ChatController extends GetxController {
   static ChatController get to => Get.find();
   List<ChatMessage> chatMessages = [];
+  final TextEditingController inputController = TextEditingController();
+  final ScrollController scrollController = ScrollController();
+  final DatabaseService databaseService = DatabaseService();
 
   @override
   Future<void> onInit() async {
     super.onInit();
-    chatMessages = await DatabaseService().getChatMessages();
+    chatMessages = await databaseService.getChatMessages();
     update();
   }
 
