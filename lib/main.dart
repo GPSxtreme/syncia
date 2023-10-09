b4b6c88a656c0612299fa96f239bf835e5f2f8d2
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-10-09 23:45:06 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:dart_openai/dart_openai.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:syncia/pages/home.dart';
+import 'app.dart';
 
 Future<void> main() async {
   await dotenv
@@ -10,29 +10,3 @@ Future<void> main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-          useMaterial3: true,
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            textStyle: const TextStyle(color: Colors.white),
-            shape: ContinuousRectangleBorder(borderRadius: BorderRadius.circular(18.0)),
-            backgroundColor: Colors.blue,
-          )
-        )
-      ),
-      debugShowCheckedModeBanner: false,
-      initialRoute: HomePage.id,
-      routes: {
-        HomePage.id: (context) => const HomePage(),
-      },
-    );
-  }
-}
