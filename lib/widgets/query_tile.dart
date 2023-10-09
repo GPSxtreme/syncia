0d7d4c58eec1faaccfb17b221dfccfbc90c5f871
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-10-09 23:45:15 +0530

    Update query_tile.dart

--- lib/widgets/query_tile.dart
+++ lib/widgets/query_tile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/services.dart';
+import 'package:syncia/widgets/markdown_response_tile.dart';
 
 class QueryTile extends StatefulWidget {
   const QueryTile(
@@ -19,6 +21,8 @@ class QueryTile extends StatefulWidget {
 }
 
 class _QueryTileState extends State<QueryTile> {
+  bool _showFormatted = false;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -26,43 +30,55 @@ class _QueryTileState extends State<QueryTile> {
       children: [
         Material(
           color: Colors.grey.withOpacity(0.15),
-          child: InkWell(
-            onTap: () {},
-            child: Container(
-              decoration: const BoxDecoration(
-                  border: Border(
-                      left: BorderSide(color: Colors.blue, width: 3.0))),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.all(12.0),
-                            child: Text(
-                              widget.query,
-                              style: const TextStyle(
-                                  fontSize: 17.0, fontWeight: FontWeight.normal),
-                            ),
-                          )),
-                    ],
-                  )
-                ],
-              ),
+          child: Container(
+            decoration: const BoxDecoration(
+                border:
+                    Border(left: BorderSide(color: Colors.blue, width: 3.0))),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                        child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: SelectableText(
+                        widget.query,
+                        style: const TextStyle(
+                            fontSize: 17.0, fontWeight: FontWeight.normal),
+                      ),
+                    )),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: IconButton(
+                          color: Colors.blue,
+                          tooltip: 'copy response',
+                          onPressed: () {
+                            Clipboard.setData(ClipboardData(text: widget.response));
+                          }, icon: const Icon(Icons.copy, color: Colors.black,)),
+                    )
+                  ],
+                )
+              ],
             ),
           ),
         ),
         ListTile(
-          contentPadding: const EdgeInsets.symmetric(horizontal: 15,vertical: 5),
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
           title: widget.isLast &&
-                  !widget.isRead // Check if it's the latest response
+                  !widget.isRead && !_showFormatted// Check if it's the latest response
               ? AnimatedTextKit(
                   animatedTexts: [
                     TypewriterAnimatedText(widget.response),
                   ],
                   totalRepeatCount: 1,
+            onFinished: (){
+              setState(() {
+                _showFormatted = true;
+              });
+            },
                 )
-              : SelectableText(widget.response),
+              : MarkdownResponseTile(response: widget.response),
         ),
       ],
     );
