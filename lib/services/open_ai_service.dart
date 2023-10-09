83b5c67507556d612bad3aaf34e337fad6c76a2f
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-10-10 00:24:09 +0530

    Update open_ai_service.dart

--- lib/services/open_ai_service.dart
+++ lib/services/open_ai_service.dart
@@ -36,17 +36,27 @@ class OpenAiService{
   }
 
   /// Returns the chat completion model with the given id
-  Future<OpenAIChatCompletionModel> chatCompletion(String prompt,String model)async{
-    return await OpenAI.instance.chat.create(
-      model: model,
-      messages: [
-        OpenAIChatCompletionChoiceMessageModel(
-          content: prompt,
-          role: OpenAIChatMessageRole.user,
-        ),
-      ],
+  Future<OpenAIChatCompletionModel> chatCompletion(
+      String content,
+      String model,
+      List<OpenAIChatCompletionChoiceMessageModel> currentChatHistory,
+      ) async {
+    // Add the new user message to the provided chat history
+    currentChatHistory.add(
+      OpenAIChatCompletionChoiceMessageModel(
+        content: content,
+        role: OpenAIChatMessageRole.user,
+      ),
     );
+
+    var response = await OpenAI.instance.chat.create(
+      model: model,
+      messages: currentChatHistory,
+    );
+
+    return response;
   }
+
 
   /// Returns the stream chat completion model with the given id
   Future<Stream<OpenAIStreamChatCompletionModel>> streamChatCompletion(String prompt,String model)async{
