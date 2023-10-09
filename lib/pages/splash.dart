bc27887caf3192e9d1c9fbcdd8c4324a1ec1e834
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-10-09 23:44:51 +0530

    Create splash.dart

---
+++ lib/pages/splash.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+import 'package:syncia/styles/app_styles.dart';
+import '../route.dart';
+import '../styles/size_config.dart';
+
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    // redirect to home page
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.offAndToNamed(Routes.homePage);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Stack(
+    children: [
+      Center(
+        child: SvgPicture.asset(
+          'assets/svgs/app_bar_logo.svg',
+          height: SizeConfig.screenWidth! * 0.5,
+          width: SizeConfig.screenWidth! * 0.5,
+          fit: BoxFit.contain,
+        ),
+      ),
+      Positioned(
+        bottom: 20,
+        left: 0,
+        right: 0,
+        child: Center(child: kDevLogo),
+      )
+    ],
+      )
+    );
+  }
+}
