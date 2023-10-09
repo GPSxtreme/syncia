c3fc4afc9d35423fffcd961e6def7f396003dce3
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-10-09 23:44:39 +0530

    Create app_styles.dart

---
+++ lib/styles/app_styles.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Widget kDevLogo = Text(
+  "GPS",
+  style:
+      GoogleFonts.fugazOne(color: Colors.black.withOpacity(0.13), fontSize: 20),
+);
