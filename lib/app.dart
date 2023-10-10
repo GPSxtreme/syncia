19fbab98b9841f6e2b474188fa98e92d234af824
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-10-10 22:03:23 +0530

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,32 +1,21 @@
-import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:syncia/pages/text_chat.dart';
+import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'controllers/theme_controller.dart';
 import 'route.dart';
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final ThemeController _themeController = Get.put(ThemeController());
+
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      theme: ThemeData(
-          useMaterial3: true,
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-          elevatedButtonTheme: ElevatedButtonThemeData(
-              style: ElevatedButton.styleFrom(
-                textStyle: const TextStyle(color: Colors.white),
-                shape: ContinuousRectangleBorder(borderRadius: BorderRadius.circular(18.0)),
-                backgroundColor: Colors.blue,
-              )
-          ),
-        floatingActionButtonTheme: const FloatingActionButtonThemeData(
-          backgroundColor: Colors.blue,
-        )
-      ),
+    return Obx(() => GetMaterialApp(
+      theme: _themeController.theme,
       debugShowCheckedModeBanner: false,
       initialRoute: Routes.splashPage,
       getPages: getPages,
-    );
+    ));
   }
 }
+
