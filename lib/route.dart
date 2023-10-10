a3ec47d3e14128e4a30371dcccc239f6eafea384
Prudhvi Suraaj <86603322+GPSxtreme@users.noreply.github.com>
2023-10-11 01:56:02 +0530

    Update route.dart

--- lib/route.dart
+++ lib/route.dart
@@ -1,24 +1,31 @@
 import 'package:get/get_navigation/src/routes/get_route.dart';
-import 'package:syncia/pages/home.dart';
+import 'package:syncia/pages/settings.dart';
 import 'package:syncia/pages/splash.dart';
 import 'package:syncia/pages/text_chat.dart';
+import 'package:syncia/pages/text_chats.dart';
 
 class Routes {
   static String splashPage = '/splash';
-  static String homePage = '/home';
+  static String textChatsPage = '/text_chats';
   static String textChatPage = '/text_chat';
+  static String settingsPage = '/settings';
 }
+
 final getPages = [
   GetPage(
     name: Routes.splashPage,
     page: () => const SplashPage(),
   ),
   GetPage(
-    name: Routes.homePage,
-    page: () => const HomePage(),
+    name: Routes.textChatsPage,
+    page: () => const TextChatsPage(),
   ),
   GetPage(
     name: Routes.textChatPage,
     page: () => const TextChatPage(),
+  ),
+  GetPage(
+    name: Routes.settingsPage,
+    page: () => const SettingsPage(),
   )
 ];
