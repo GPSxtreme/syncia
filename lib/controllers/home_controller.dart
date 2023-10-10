b7b311b9e76153490813d03be1a9cdf267c36b07
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-10-10 19:34:06 +0530

    Update home_controller.dart

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -1,8 +1,8 @@
 import 'package:get/get.dart';
 import 'package:syncia/services/local_database_service.dart';
-import 'package:uuid/uuid.dart';
 import '../models/chat_room_data.dart';
 import '../route.dart';
+import 'package:flutter/material.dart';
 
 class HomeController extends GetxController {
   static HomeController get to => Get.find();
@@ -21,10 +21,58 @@ class HomeController extends GetxController {
   }
 
   Future<void> createTextChatRoom() async {
-    await databaseService.createChatRoom(const Uuid().v4()).then(
-        (id) => Get.toNamed(Routes.textChatPage, arguments: {'roomId': id}));
-    await getChatRooms();
+    TextEditingController roomNameController = TextEditingController();
+
+
+    // Function to proceed with room creation
+    Future<void> proceedWithCreation() async {
+      if (roomNameController.text.isNotEmpty) {
+        int roomId = await databaseService.createChatRoom(roomNameController.text);
+        Get.toNamed(Routes.textChatPage, arguments: {'roomId': roomId, 'isNew': true});
+        await getChatRooms();
+        Get.back();  // Close the dialog
+      } else {
+        Get.snackbar('Error', 'Room name cannot be empty!');
+      }
+    }
+
+    Get.defaultDialog(
+      title: 'Create chat room',
+      radius: 12,
+      titlePadding: const EdgeInsets.only(top: 18.0),
+      content: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: roomNameController,
+              decoration: const InputDecoration(
+                labelText: 'Room Name',
+                labelStyle: TextStyle(fontSize: 14),
+                border: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.black,
+                    width: 1,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ElevatedButton(
+                  onPressed: proceedWithCreation,
+                  child: const Text('Create',style: TextStyle(color: Colors.white),),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
+
   Future<void> deleteChatRoom(int id) async {
     await databaseService.deleteChatRoom(id).then(
         (_){
