293331a9f03d971375d3a9446be3576ba0fc1803
Prudhvi Suraaj <86603322+GPSxtreme@users.noreply.github.com>
2023-10-12 02:01:17 +0530

    Update theme_controller.dart

--- lib/controllers/theme_controller.dart
+++ lib/controllers/theme_controller.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:get/get.dart';
 
 import '../styles/themes.dart';
 
+Brightness brightness =
+    SchedulerBinding.instance.platformDispatcher.platformBrightness;
+
 class ThemeController extends GetxController {
   static ThemeController get to => Get.find();
-  RxBool isDarkTheme = false.obs; // by default set to light theme
+  RxBool isDarkTheme =
+      (brightness == Brightness.dark).obs; // by default set to light theme
 
   ThemeData get theme => isDarkTheme.value ? darkTheme : lightTheme;
 
