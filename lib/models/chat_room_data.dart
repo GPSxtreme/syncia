15c7f435421a017b66f13c3362d143bc41073ce6
Prudhvi Suraaj <86603322+GPSxtreme@users.noreply.github.com>
2023-10-11 17:01:41 +0530

    Update chat_room_data.dart

--- lib/models/chat_room_data.dart
+++ lib/models/chat_room_data.dart
@@ -2,11 +2,12 @@ class ChatRoomData {
   final int id;
   final String name;
   final String createdOn;
-
+  final String modelName;
   ChatRoomData({
     required this.id,
     required this.name,
     required this.createdOn,
+    required this.modelName,
   });
 
   Map<String, dynamic> toMap() {
@@ -14,6 +15,7 @@ class ChatRoomData {
       'id': id,
       'name': name,
       'createdOn': createdOn,
+      'modelName': modelName,
     };
   }
 
@@ -22,6 +24,7 @@ class ChatRoomData {
       id: map['id'],
       name: map['name'],
       createdOn: map['createdOn'],
+      modelName: map['modelName'],
     );
   }
 }
