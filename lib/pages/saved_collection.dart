6735c5a7799ea09e83405874af08a586d10e9b18
Prudhvi Suraaj <86603322+GPSxtreme@users.noreply.github.com>
2023-10-14 20:36:38 +0530

    Create saved_collection.dart

---
+++ lib/pages/saved_collection.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SavedCollectionPage extends StatelessWidget {
+  const SavedCollectionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
