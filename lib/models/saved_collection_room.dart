3cb539be6be2ddfc9278a98e553c3d89ca0c1c3a
Prudhvi Suraaj <86603322+GPSxtreme@users.noreply.github.com>
2023-10-15 22:32:51 +0530

    Update saved_collection_room.dart

--- lib/models/saved_collection_room.dart
+++ lib/models/saved_collection_room.dart
@@ -1,23 +1,20 @@
 class SavedCollectionRoom {
   final int id;
   final String name;
+  final String createdOn;
 
-  SavedCollectionRoom({
-    required this.id,
-    required this.name,
-  });
+  SavedCollectionRoom(
+      {required this.id, required this.name, required this.createdOn});
 
   Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'name': name,
-    };
+    return {'id': id, 'name': name, 'created': createdOn};
   }
 
   factory SavedCollectionRoom.fromMap(Map<String, dynamic> map) {
     return SavedCollectionRoom(
       id: map['id'] is int ? map['id'] : int.parse(map['id']),
       name: map['name']?.toString() ?? '',
+      createdOn: map['created']?.toString() ?? '',
     );
   }
 }
