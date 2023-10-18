42996fa65942c5f3ed34cc1bda7ad4de1846af8e
Prudhvi Suraaj <86603322+GPSxtreme@users.noreply.github.com>
2023-10-19 01:09:33 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,7 @@
-import 'package:dart_openai/dart_openai.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:syncia/services/open_ai_service.dart';
 import 'app.dart';
 
-Future<void> main() async {
-  await dotenv
-      .load(fileName: ".env")
-      .then((_) => OpenAI.apiKey = dotenv.env['API_KEY']!);
-  await OpenAiService.init();
+main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
