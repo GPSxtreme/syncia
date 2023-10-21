2f65a3ca81b4bc8765bb6b0e9bbbf443697e235c
Prudhvi Suraaj <86603322+GPSxtreme@users.noreply.github.com>
2023-10-21 20:21:39 +0530

    Update settings_controller.dart

--- lib/controllers/settings_controller.dart
+++ lib/controllers/settings_controller.dart
@@ -2,23 +2,35 @@ import 'package:dart_openai/dart_openai.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:syncia/services/open_ai_service.dart';
+import 'package:url_launcher/url_launcher.dart';
 import '../route.dart';
 
 class SettingsController extends GetxController {
   static SettingsController get to => Get.find();
   final _storage = const FlutterSecureStorage();
   final apiKeyController = TextEditingController();
+  final RxString version = RxString('');
+  final RxString buildVersion = RxString('');
+
   @override
   void onInit() async {
     super.onInit();
     await loadApiKey();
+    await _getAppVersionData();
   }
 
   @override
   void dispose() {
     apiKeyController.dispose();
     super.dispose();
+  }
+
+  Future<void> _getAppVersionData() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    version.value = packageInfo.version;
+    buildVersion.value = packageInfo.buildNumber;
   }
 
   Future<void> loadApiKey() async {
@@ -55,6 +67,18 @@ class SettingsController extends GetxController {
     } catch (e) {
       Get.snackbar(
           'Error', 'Failed to set api key.\nClick here to view full log',
+          icon: const Icon(Icons.error), onTap: (_) {
+        Get.toNamed(Routes.viewErrorPage, arguments: {'log': e.toString()});
+      });
+    }
+  }
+
+  Future<void> launchLink(String url) async {
+    try {
+      final Uri uri = Uri.parse(url);
+      await launchUrl(uri, mode: LaunchMode.externalApplication);
+    } catch (e) {
+      Get.snackbar('Error', 'Error launching link\nClick here to view full log',
           icon: const Icon(Icons.error), onTap: (_) {
         Get.toNamed(Routes.viewErrorPage, arguments: {'log': e.toString()});
       });
