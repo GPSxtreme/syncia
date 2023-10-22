70b37785f7916ce943ef664ccc3ebba9df34f833
Prudhvi Suraaj <86603322+GPSxtreme@users.noreply.github.com>
2023-10-22 13:02:02 +0530

    Update app_styles.dart

--- lib/styles/app_styles.dart
+++ lib/styles/app_styles.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:syncia/controllers/settings_controller.dart';
 import 'package:syncia/controllers/theme_controller.dart';
 
 Widget kDevLogo = Obx(() => Text(
@@ -11,3 +12,17 @@ Widget kDevLogo = Obx(() => Text(
               : Colors.black12,
           fontSize: 20),
     ));
+
+Widget kDevLogoWithVersion = Column(
+  children: [
+    kDevLogo,
+    Obx(() => Text(
+          SettingsController.to.version.value,
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: ThemeController.to.isDarkTheme.value
+                  ? Colors.white12
+                  : Colors.black12),
+        ))
+  ],
+);
