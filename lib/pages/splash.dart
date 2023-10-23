3ad8178412e364781630b749f39879a004c73b63
Prudhvi Suraaj <86603322+GPSxtreme@users.noreply.github.com>
2023-10-23 17:48:30 +0530

    Update splash.dart

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
+import 'package:syncia/controllers/image_chats_controller.dart';
 import 'package:syncia/styles/app_styles.dart';
+import '../controllers/chats_controller.dart';
+import '../controllers/saved_collections_controller.dart';
 import '../controllers/settings_controller.dart';
 import '../route.dart';
 import '../styles/size_config.dart';
@@ -17,12 +20,20 @@ class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
     super.initState();
-    // redirect to home page
+    _performInitState();
+  }
+
+  _performInitState() async {
     Get.put(SettingsController(), permanent: true);
-    Future.delayed(const Duration(seconds: 3), () async {
-      if (SettingsController.to.apiKeyController.text.isNotEmpty) {
-        Get.offAndToNamed(Routes.textChatsPage);
-      }
+    Get.put(ChatsController(), permanent: true);
+    Get.put(ImageChatsController(), permanent: true);
+    Get.put(SavedCollectionsController(), permanent: true);
+    await SettingsController.inAppUpdate().then((_) {
+      Future.delayed(const Duration(seconds: 2), () async {
+        if (SettingsController.to.apiKeyController.text.isNotEmpty) {
+          Get.offAndToNamed(Routes.textChatsPage);
+        }
+      });
     });
   }
 
