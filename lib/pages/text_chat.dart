6737b04fc8d6cdf0af23d771cf61fc8663486f8b
Prudhvi Suraaj <86603322+GPSxtreme@users.noreply.github.com>
2023-10-26 22:43:19 +0530

    Update text_chat.dart

--- lib/pages/text_chat.dart
+++ lib/pages/text_chat.dart
@@ -60,12 +60,10 @@ class _TextChatPageState extends State<TextChatPage> {
                       ),
                     )
                   : controller.chatMessages.isEmpty && controller.initialized
-                      ? Positioned(
-                          top: 0,
-                          bottom: 0,
-                          left: 0,
-                          right: 0,
+                      ? Align(
+                          alignment: Alignment.center,
                           child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
                             children: [
                               Center(
                                   child: Image.asset(
