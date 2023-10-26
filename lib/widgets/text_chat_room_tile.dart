0222f91fa141724973036f3f5a84b1b43238da48
Prudhvi Suraaj <86603322+GPSxtreme@users.noreply.github.com>
2023-10-26 22:43:33 +0530

    Update text_chat_room_tile.dart

--- lib/widgets/text_chat_room_tile.dart
+++ lib/widgets/text_chat_room_tile.dart
@@ -3,39 +3,63 @@ import 'package:intl/intl.dart';
 import 'package:syncia/controllers/chats_controller.dart';
 import 'package:syncia/models/chat_room_data.dart';
 import 'package:get/get.dart';
-
 import '../route.dart';
+import 'common_widgets.dart';
 
 class TextChatRoomTile extends StatelessWidget {
   const TextChatRoomTile({super.key, required this.chatRoomData});
   final ChatRoomData chatRoomData;
+
+  _showModalBottomSheet(BuildContext context) => showModalBottomSheet(
+      showDragHandle: true,
+      context: context,
+      builder: (context) {
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 30),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                onTap: () async {
+                  bool? response = await CommonWidgets.commonAlertDialog(
+                      context,
+                      title: 'Delete room?',
+                      body: 'This action is irreversible',
+                      agreeLabel: "Yes",
+                      denyLabel: "No");
+                  if (response == true) {
+                    await ChatsController.to.deleteChatRoom(chatRoomData.id);
+                  }
+                  Get.back();
+                },
+                leading: const Icon(Icons.delete),
+                title: const Text('Delete room'),
+              )
+            ],
+          ),
+        );
+      });
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(
-        chatRoomData.name,
-        style: const TextStyle(fontSize: 18),
-      ),
-      onTap: () {
-        // go to text chat page
-        Get.toNamed(Routes.textChatPage, arguments: chatRoomData.toMap());
-      },
-      subtitle: Padding(
-        padding: const EdgeInsets.only(top: 3),
-        child: Text(
-          'Created on : ${DateFormat('d MMMM, h:mm a').format(DateTime.parse(chatRoomData.createdOn).toLocal())}',
-          style: const TextStyle(fontSize: 15),
+        title: Text(
+          chatRoomData.name,
+          style: const TextStyle(fontSize: 18),
         ),
-      ),
-      trailing: IconButton(
-        onPressed: () async {
-          await ChatsController.to.deleteChatRoom(chatRoomData.id);
+        onTap: () {
+          // go to text chat page
+          Get.toNamed(Routes.textChatPage, arguments: chatRoomData.toMap());
         },
-        icon: const Icon(
-          Icons.delete,
-          size: 30,
+        subtitle: Padding(
+          padding: const EdgeInsets.only(top: 3),
+          child: Text(
+            'Created on : ${DateFormat('d MMMM, h:mm a').format(DateTime.parse(chatRoomData.createdOn).toLocal())}',
+            style: const TextStyle(fontSize: 15),
+          ),
         ),
-      ),
-    );
+        onLongPress: () {
+          _showModalBottomSheet(context);
+        });
   }
 }
