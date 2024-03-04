f4cff606a509b3a0e0308f148ccb920c69852fc3
Prudhvi Suraaj <86603322+GPSxtreme@users.noreply.github.com>
2024-03-04 10:50:46 +0530

    Update floating action button based on settings availability

--- lib/pages/image_chats.dart
+++ lib/pages/image_chats.dart
@@ -5,6 +5,7 @@ import 'package:syncia/controllers/theme_controller.dart';
 import 'package:syncia/widgets/app_drawer.dart';
 import 'package:syncia/widgets/create_image_chat_room_dialog_box.dart';
 import 'package:syncia/widgets/image_chat_room_tile.dart';
+import 'package:syncia/controllers/settings_controller.dart';
 
 class ImageChatsPage extends StatelessWidget {
   const ImageChatsPage({super.key});
@@ -18,21 +19,23 @@ class ImageChatsPage extends StatelessWidget {
         elevation: 0.5,
       ),
       drawer: const AppDrawer(),
-      floatingActionButton: FloatingActionButton(
-          child: Obx(() => Icon(
-                Icons.add,
-                color: ThemeController.to.isDarkTheme.value
-                    ? Colors.blue
-                    : Colors.white,
-                size: 40,
-              )),
-          onPressed: () => showDialog(
-                context: context,
-                barrierDismissible: true,
-                builder: (context) {
-                  return const CreateImageChatRoomDialogBox();
-                },
-              )),
+      floatingActionButton: SettingsController.to.models.isNotEmpty
+          ? FloatingActionButton(
+              child: Obx(() => Icon(
+                    Icons.add,
+                    color: ThemeController.to.isDarkTheme.value
+                        ? Colors.blue
+                        : Colors.white,
+                    size: 40,
+                  )),
+              onPressed: () => showDialog(
+                    context: context,
+                    barrierDismissible: true,
+                    builder: (context) {
+                      return const CreateImageChatRoomDialogBox();
+                    },
+                  ))
+          : null,
       body: GetBuilder<ImageChatsController>(
         builder: (controller) {
           if (controller.chatRooms.isNotEmpty) {
@@ -46,9 +49,11 @@ class ImageChatsPage extends StatelessWidget {
               },
             );
           } else if (controller.chatRooms.isEmpty && controller.initialized) {
-            return const Center(
+            return Center(
               child: Text(
-                'No chats available\nCreate new chat by pressing add icon below',
+                SettingsController.to.models.isEmpty
+                    ? "Error loading models!\nPlease check your api key/internet and try again."
+                    : 'No chats available\nCreate new chat by pressing add icon below',
                 style: TextStyle(fontSize: 15),
                 textAlign: TextAlign.center,
               ),
