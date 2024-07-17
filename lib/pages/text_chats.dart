4526e6dae66d9305c290a10c8ef2d26543209ed0
Prudhvi Suraaj <86603322+GPSxtreme@users.noreply.github.com>
2024-07-17 17:46:49 +0530

    Update text and icon styling in TextChatsPage

--- lib/pages/text_chats.dart
+++ lib/pages/text_chats.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:syncia/controllers/chats_controller.dart';
 import 'package:syncia/controllers/settings_controller.dart';
-import 'package:syncia/controllers/theme_controller.dart';
 import 'package:syncia/widgets/app_drawer.dart';
 import 'package:syncia/widgets/create_text_chat_room_dialog_box.dart';
 import '../widgets/text_chat_room_tile.dart';
@@ -21,18 +20,18 @@ class _TextChatsPageState extends State<TextChatsPage> {
       drawer: const AppDrawer(),
       appBar: AppBar(
         centerTitle: true,
-        title: const Text('All chats'),
+        title: Text(
+          'All chats',
+          style: Theme.of(context).textTheme.headlineSmall,
+        ),
         elevation: 0.5,
       ),
       floatingActionButton: SettingsController.to.models.isNotEmpty
           ? FloatingActionButton(
-              child: Obx(() => Icon(
-                    Icons.add,
-                    color: ThemeController.to.isDarkTheme.value
-                        ? Colors.blue
-                        : Colors.white,
-                    size: 40,
-                  )),
+              child: const Icon(
+                Icons.add,
+                size: 40,
+              ),
               onPressed: () => showDialog(
                     context: context,
                     barrierDismissible: true,
@@ -56,14 +55,38 @@ class _TextChatsPageState extends State<TextChatsPage> {
               },
             );
           } else if (controller.chatRooms.isEmpty && controller.initialized) {
-            return Center(
-              child: Text(
-                SettingsController.to.models.isEmpty
-                    ? "Error loading models!\nPlease check your api key/internet and try again."
-                    : 'No chats available\nCreate new chat by pressing add icon below',
-                style: TextStyle(fontSize: 15),
-                textAlign: TextAlign.center,
-              ),
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Center(
+                  child: Material(
+                    shape: const CircleBorder(),
+                    color: Theme.of(context).colorScheme.secondary,
+                    child: Padding(
+                      padding: const EdgeInsets.all(24),
+                      child: Icon(
+                        SettingsController.to.models.isEmpty
+                            ? Icons.error
+                            : Icons.chat,
+                        color: Theme.of(context).colorScheme.surfaceDim,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                  child: Text(
+                    SettingsController.to.models.isEmpty
+                        ? "Error loading models. Please check your api key/internet and try again."
+                        : 'No chats available. Create new chat by pressing add icon below',
+                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                        color: Theme.of(context).colorScheme.surfaceDim),
+                    textAlign: TextAlign.center,
+                  ),
+                )
+              ],
             );
           } else {
             return const Center(
