da82709d4244b633818a35c25db4d19f63982068
Prudhvi Suraaj <86603322+GPSxtreme@users.noreply.github.com>
2024-07-17 17:47:29 +0530

    Refactor text chat textfield theming and UI structure

--- lib/widgets/text_chat_textfield.dart
+++ lib/widgets/text_chat_textfield.dart
@@ -10,106 +10,109 @@ class TextChatTextField extends StatelessWidget {
   final controller = ChatController.to;
   @override
   Widget build(BuildContext context) {
-    return Obx(
-        (){
-          // get colors according to theme
-          final containerBackgroundColor = ThemeController.to.isDarkTheme.value ? HexColor('#212121') : Colors.white;
-          final containerBorderColor = ThemeController.to.isDarkTheme.value ? Colors.white10 : Colors.grey.withOpacity(0.3);
-          final hintTextColor = ThemeController.to.isDarkTheme.value ? Colors.white70 : Colors.grey.withOpacity(0.8);
-          final elevatedButtonColor = ThemeController.to.isDarkTheme.value ? Colors.white12 : Colors.blue;
-          final elevatedButtonTextColor = ThemeController.to.isDarkTheme.value ? Colors.blue : Colors.white;
-          return Container(
-            padding:
-            const EdgeInsets.symmetric(horizontal: 6, vertical: 6),
-            decoration: BoxDecoration(
-              color: containerBackgroundColor,
-              border: Border.all(width: 1.0,color: containerBorderColor),
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
+    return Obx(() {
+      // get colors according to theme
+      final containerBackgroundColor = ThemeController.to.isDarkTheme.value
+          ? HexColor('#212121')
+          : Colors.white;
+      final containerBorderColor = ThemeController.to.isDarkTheme.value
+          ? Colors.white10
+          : Colors.grey.withOpacity(0.3);
+      final hintTextColor = ThemeController.to.isDarkTheme.value
+          ? Colors.white70
+          : Colors.grey.withOpacity(0.8);
+      final elevatedButtonColor =
+          ThemeController.to.isDarkTheme.value ? Colors.white12 : Colors.blue;
+      final elevatedButtonTextColor =
+          ThemeController.to.isDarkTheme.value ? Colors.blue : Colors.white;
+      return Container(
+        padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 6),
+        decoration: BoxDecoration(
+          color: containerBackgroundColor,
+          border: Border.all(width: 1.0, color: containerBorderColor),
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
               children: [
-                Row(
-                  children: [
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.only(
-                            left: 8.0, bottom: 4.0),
-                        child: TextField(
-                          controller: controller.inputController,
-                          minLines: 1,
-                          maxLines: 8,
-                          decoration: InputDecoration(
-                            hintText: "Type your message here..",
-                            hintStyle: TextStyle(
-                                color: hintTextColor),
-                            border: InputBorder.none,
-                          ),
-                        ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 8.0, bottom: 4.0),
+                    child: TextField(
+                      controller: controller.inputController,
+                      minLines: 1,
+                      maxLines: 8,
+                      decoration: InputDecoration(
+                        hintText: "Type your message here..",
+                        hintStyle: TextStyle(color: hintTextColor),
+                        border: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        enabledBorder: InputBorder.none,
+                        errorBorder: InputBorder.none,
+                        disabledBorder: InputBorder.none,
+                        focusedErrorBorder: InputBorder.none,
                       ),
                     ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    const SizedBox(
-                      width: 8,
-                    ),
-                    Obx(() => Text(
-                      '${controller.characterCount.value} / ${MAX_CHAR.toString()}',
-                      style: TextStyle(
-                          fontSize: 12.0,
-                          color:
-                          controller.characterCount > MAX_CHAR
-                              ? Colors.red
-                              : null),
-                    )),
-                    const Spacer(),
-                    Obx(() => ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: elevatedButtonColor,
-                      ),
-                      onPressed: !controller
-                          .isSendingMessage.value &&
-                          controller.characterCount < MAX_CHAR
-                          ? () {
-                        // close keyboard
-                        FocusManager.instance.primaryFocus
-                            ?.unfocus();
-                        // send message
-                        controller.sendChatMessage();
-                      }
-                          : null,
-                      child: !controller.isSendingMessage.value
-                          ? Row(
-                        children: [
-                          Text(
-                            'Send',
-                            style: TextStyle(
-                              color: elevatedButtonTextColor,
-                            )
-                          ),
-                          const SizedBox(
-                            width: 3,
-                          ),
-                          Icon(
-                            Icons.send_rounded,
-                            size: 15,
-                            color: elevatedButtonTextColor,
-                          )
-                        ],
-                      )
-                          : const SpinKitPulse(
-                        color: Colors.white,
-                        size: 25,
-                      ),
-                    ))
-                  ],
+                  ),
                 ),
               ],
             ),
-          );
-        }
-    );
+            Row(
+              children: [
+                const SizedBox(
+                  width: 8,
+                ),
+                Obx(() => Text(
+                      '${controller.characterCount.value} / ${MAX_CHAR.toString()}',
+                      style: TextStyle(
+                          fontSize: 12.0,
+                          color: controller.characterCount > MAX_CHAR
+                              ? Colors.red
+                              : null),
+                    )),
+                const Spacer(),
+                Obx(() => ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: elevatedButtonColor,
+                      ),
+                      onPressed: !controller.isSendingMessage.value &&
+                              controller.characterCount < MAX_CHAR
+                          ? () {
+                              // close keyboard
+                              FocusManager.instance.primaryFocus?.unfocus();
+                              // send message
+                              controller.sendChatMessage();
+                            }
+                          : null,
+                      child: !controller.isSendingMessage.value
+                          ? Row(
+                              children: [
+                                Text('Send',
+                                    style: TextStyle(
+                                      color: elevatedButtonTextColor,
+                                    )),
+                                const SizedBox(
+                                  width: 3,
+                                ),
+                                Icon(
+                                  Icons.send_rounded,
+                                  size: 15,
+                                  color: elevatedButtonTextColor,
+                                )
+                              ],
+                            )
+                          : const SpinKitPulse(
+                              color: Colors.white,
+                              size: 25,
+                            ),
+                    ))
+              ],
+            ),
+          ],
+        ),
+      );
+    });
   }
 }
